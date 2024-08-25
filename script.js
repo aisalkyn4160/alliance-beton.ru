01cc1e6f09926588aec0d53c802e93d53e859feb
aisalkyn4160 <aisalkyn.4160@gmail.com>
2024-08-26 01:12:53 +0300

    correcting hover effect

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const closeFormBtn = document.querySelector('.close-form')
 
 popupShowBtns.forEach(item => {
     item.addEventListener('click', () => {
-        popup.classList.add('show-popup')
+             popup.classList.add('show-popup')
         document.body.classList.add('no-scroll')
     })
 })
@@ -62,4 +62,39 @@ document.addEventListener('click', (event) => {
         popup.classList.remove('show-popup')
         document.body.classList.remove('no-scroll')
     }
+});
+
+
+// ---------------------------------advanced search----------------------------------
+
+document.addEventListener('DOMContentLoaded', function() {
+    const advantageItems = document.querySelectorAll('.advantages-list li');
+
+    advantageItems.forEach(item => {
+        const img = item.querySelector('.active-img');
+        const defImg = item.querySelector('.default-img');
+        defImg.style.opacity = '1';
+        defImg.style.zIndex = '1';
+       
+        item.addEventListener('mouseenter', function(event) {
+            // Проверяем, что курсор наведен именно на li, а не на вложенные элементы
+            if (event.target === item && img) {
+                img.style.opacity = '1';
+                img.style.zIndex = '1';
+                img.style.transition = 'opacity 0.3s ease-out';
+            }
+        });
+
+        item.addEventListener('mouseleave', function(event) {
+            // Проверяем, что курсор ушел именно с li, а не с вложенных элементов
+            if (event.target === item && img) {
+                img.style.opacity = '0';
+                img.style.zIndex = '-1';
+                img.style.transition = 'all 0.3s ease-out';
+                defImg.style.opacity = '1';
+                defImg.style.zIndex = '1';
+                defImg.style.transition = 'opacity 0.3s ease-out';
+            }
+        });
+    });
 });
